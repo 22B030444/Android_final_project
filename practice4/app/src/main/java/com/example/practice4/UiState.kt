25a815520d9c21a981f82a11cee292143b360800
Жасмин <zh_suleimenova@kbtu.kz>
2025-11-17 00:32:34 +0500

    delate

--- practice4/app/src/main/java/com/example/practice4/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.practice4
-
-sealed class UiState {
-    object Loading : UiState()
-    data class Success(val tracks: List<Track>) : UiState()
-    data class Error(val message: String) : UiState()
-}
